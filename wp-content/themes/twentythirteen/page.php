cc928bab1b40732746e02477b7be6695eb669e8a
Beth Binkovitz <beth@softpixel.com>
2014-03-29 22:53:49 -0500

    installed user role plugin, hacked theme

--- wp-content/themes/twentythirteen/page.php
+++ wp-content/themes/twentythirteen/page.php
@@ -12,6 +12,9 @@
  */
 
 get_header(); ?>
+  <?php if ( is_front_page() ) : ?>
+<div id="front">
+  <?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
@@ -27,7 +30,24 @@ get_header(); ?>
 						</div>
 						<?php endif; ?>
 
-						<h1 class="entry-title"><?php the_title(); ?></h1>
+            <?php if ( is_front_page() ) : ?>
+						<h1 class="entry-title" style="margin:auto; text-align:center;"><?php
+              $nouns = array(
+                'Strength',
+                'Power',
+                'Safety',
+                'Otters',
+                'Glitter',
+                'Magic',
+                'Fierceness',
+              );
+              $count = count($nouns);
+              $num = rand(0, $count-1);
+              $noun = $nouns[$num];
+              print("<div class=\"pink\">$noun</div>");
+            ?> in numbers
+            </h1>
+            <?php endif; ?>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
@@ -46,5 +66,6 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+  <?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
