d092aa87f20ff7dfcc005d4b3060db553ef02310
Beth Binkovitz <beth@softpixel.com>
2014-03-30 11:22:33 -0500

    tweaked theme

--- wp-content/themes/twentythirteen/sidebar-main.php
+++ wp-content/themes/twentythirteen/sidebar-main.php
@@ -9,7 +9,7 @@
  * @since Twenty Thirteen 1.0
  */
 
-if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+if ( is_active_sidebar( 'sidebar-1' ) && !is_front_page()) : ?>
 	<div id="secondary" class="sidebar-container" role="complementary">
 		<div class="widget-area">
 			<?php dynamic_sidebar( 'sidebar-1' ); ?>
